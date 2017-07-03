343664a4ef605c85ca2f8232d099ff5c3c2fbf8c
vikramorochi <vikramorochi@icloud.com>
2017-07-03 17:53:56 +0800

    2017/07/03-17:53
    重新弄了一下

---
+++ LoveTravel/LoveTravel/MainViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MainViewController.swift
+//  LoveTravel
+//
+//  Created by stu on 2017/7/3.
+//  Copyright © 2017年 Alex. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.addChildStoryboardWithName(name: "Home")
+        self.addChildStoryboardWithName(name: "Destination")
+        self.addChildStoryboardWithName(name: "Mall")
+        self.addChildStoryboardWithName(name: "Discuss")
+        self.addChildStoryboardWithName(name: "Me")
+        
+        
+    }
+    
+    func addChildStoryboardWithName(name:String) -> Void {
+        let childStoryboard = UIStoryboard(name: name, bundle: nil).instantiateInitialViewController()
+        self.addChildViewController(childStoryboard!)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
